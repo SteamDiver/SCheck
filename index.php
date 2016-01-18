6dedb004c0193a5cab646b63b16c6acb924ceed1
SteamDiver <steamdiver@yandex.ru>
2016-01-18 18:33:10 +0300

    Example

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SCheck</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div align=center>
+    <h2>Enter your text into the form</h2>
+    <form name="form" accept-charset="utf-8" method="POST" action="index.php">
+        <textarea wrap="hard" id="text" name="text" cols="150" rows="20"
+                  title="Enter your text here"><? echo($_POST["text"]) ?></textarea>
+        <br>
+        <input type=submit value="Check">
+        <INPUT class="btn" value="Clear" type="submit" onclick="document.getElementById('text').value='';"/>
+    </form>
+    <br>
+    <?php include_once("SCheck.php"); ?>
+    <form name="err_form" accept-charset="utf-8" method="POST" action="SCheck.php">
+        <textarea name="err" id="err" cols="100" rows="10" title="Error list"><?php
+            foreach($err as $error){
+                echo "$error";
+            }
+            ?></textarea>
+        <br>
+</div>
+<hr>
+<div class="checkedtextdiv">
+    <?php
+    if (!empty($output)) {
+        echo($output);
+    }
+    ?>
+</div>
+</body>
+</html>
